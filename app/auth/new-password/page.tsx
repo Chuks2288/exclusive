5f3e639ab8e9a9cd63e194b269519f86edc1eae3
Dumbiri Chuks <dumbirichuks@gmail.com>
2024-09-28 11:41:56 +0100

    updated

--- app/auth/new-password/page.tsx
+++ app/auth/new-password/page.tsx
@@ -1,13 +1,18 @@
 import { Logo } from "@/components/logo"
 
 import { NewPasswordForm } from "./_components/new-password-form"
+import { Suspense } from "react"
 
 const NewPasswordPage = () => {
     return (
-        <main className="flex flex-row justify-center min-h-screen w-full">
-            <NewPasswordForm />
-            <Logo />
-        </main>
+        <main className="">
+            <Suspense fallback={<div>Loading...</div>}>
+                <main className="flex flex-row justify-center min-h-screen w-full">
+                    <NewPasswordForm />
+                    <Logo />
+                </main>
+            </Suspense>
+        </main >
     )
 }
 

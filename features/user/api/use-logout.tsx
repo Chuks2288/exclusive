53be632e6996e87bfca529e773808f68c13f9296
Dumbiri Chuks <dumbirichuks@gmail.com>
2024-09-29 13:34:28 +0100

    updated the use-logout api

--- features/user/api/use-logout.tsx
+++ features/user/api/use-logout.tsx
@@ -1,16 +1,19 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { logout } from '@/actions/user/logout';
 import { toast } from 'sonner';
+import { useRouter } from "next/navigation";
 
 // import { redirect, useRouter } from 'next/navigation';
 
 export const useLogout = () => {
     const queryClient = useQueryClient();
+    const router = useRouter()
 
     const mutation = useMutation({
         mutationFn: logout,
         onSuccess: () => {
             toast.success('Logged out successfully');
+            router.push("/");
             queryClient.invalidateQueries({ queryKey: ["user"] });
 
         },
